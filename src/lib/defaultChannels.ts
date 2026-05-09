1468f471c945a303b3348a6b81a0fe8ef2082d61
Jorge Alonso <jalonsomerchan@gmail.com>
2026-05-09 19:15:40 +0200

    Add default TV channel list

---
+++ src/lib/defaultChannels.ts
@@ -0,0 +1,19 @@
+import type { DefaultChannel } from './types'
+
+export const defaultChannels: DefaultChannel[] = [
+  { label: 'La 1', aliases: ['la1', 'la1es', 'tve1', 'launo'] },
+  { label: 'La 2', aliases: ['la2', 'la2es', 'tve2', 'lados'] },
+  { label: 'Antena 3', aliases: ['antena3', 'antena3es', 'a3'] },
+  { label: 'Cuatro', aliases: ['cuatro', 'cuatroes'] },
+  { label: 'Telecinco', aliases: ['telecinco', 'telecincoes', 't5'] },
+  { label: 'laSexta', aliases: ['lasexta', 'lasextaes', 'sexta'] },
+  { label: 'Canal Extremadura', aliases: ['canalextremadura', 'extremadura', 'cextremadura'] },
+  { label: 'Neox', aliases: ['neox', 'neoxes'] },
+  { label: 'Nova', aliases: ['nova', 'novaes'] },
+  { label: 'Mega', aliases: ['mega', 'megaes'] },
+  { label: 'TRECE TV', aliases: ['trecetv', 'trece', '13tv', '13tves'] },
+  { label: 'DMAX', aliases: ['dmax', 'dmaxes'] },
+  { label: 'DKISS', aliases: ['dkiss', 'dkisses'] },
+  { label: 'Divinity', aliases: ['divinity', 'divinityes', 'divnity'] },
+  { label: 'Atreseries', aliases: ['atreseries', 'atreserieses', 'atreseriesinternacional'] },
+]
