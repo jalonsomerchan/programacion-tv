c21fffbd5899761935c15fdfd610c9c0a07c895a
Jorge Alonso <jalonsomerchan@gmail.com>
2026-05-10 12:45:49 +0200

    Restaurar canales recomendados y añadir RTVE

--- src/lib/defaultChannels.ts
+++ src/lib/defaultChannels.ts
@@ -3,9 +3,22 @@ import type { DefaultChannel } from './types'
 export const defaultChannels: DefaultChannel[] = [
   { label: 'La 1', aliases: ['la1', 'la1es', 'la1hd', 'la1uhd', 'tve1', 'tve1es', 'launo'] },
   { label: 'La 2', aliases: ['la2', 'la2es', 'la2hd', 'tve2', 'tve2es', 'lados'] },
+  { label: 'Antena 3', aliases: ['antena3', 'antena3es', 'a3'] },
+  { label: 'Cuatro', aliases: ['cuatro', 'cuatroes'] },
+  { label: 'Telecinco', aliases: ['telecinco', 'telecincoes', 't5'] },
+  { label: 'laSexta', aliases: ['lasexta', 'lasextaes', 'sexta'] },
+  { label: 'Canal Extremadura', aliases: ['canalextremadura', 'extremadura', 'cextremadura'] },
   { label: 'Teledeporte', aliases: ['teledeporte', 'teledeportees', 'tdp', 'tdpes', 'teledeportehd'] },
   {
     label: 'Canal 24 horas',
     aliases: ['canal24horas', 'canal24horases', '24horas', '24horases', '24h', '24hes', 'rtve24h', 'rtve24horas'],
   },
+  { label: 'Neox', aliases: ['neox', 'neoxes'] },
+  { label: 'Nova', aliases: ['nova', 'novaes'] },
+  { label: 'Mega', aliases: ['mega', 'megaes'] },
+  { label: 'TRECE TV', aliases: ['trecetv', 'trece', '13tv', '13tves'] },
+  { label: 'DMAX', aliases: ['dmax', 'dmaxes'] },
+  { label: 'DKISS', aliases: ['dkiss', 'dkisses'] },
+  { label: 'Divinity', aliases: ['divinity', 'divinityes', 'divnity'] },
+  { label: 'Atreseries', aliases: ['atreseries', 'atreserieses', 'atreseriesinternacional'] },
 ]
